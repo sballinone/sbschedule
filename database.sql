139f0ee39fc323eb571f029bb0dbd444a4dc96d8
Saskia Brueckner <info@saskiabrueckner.com>
2020-09-26 13:55:50 +0200

    0.1.0 demo data

---
+++ database.sql
@@ -0,0 +1,43 @@
+--- Tables
+
+CREATE TABLE `appointment` (
+  `appointmentid` int(9) NOT NULL,
+  `userid` int(9) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `apptDate` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime DEFAULT NULL,
+  `response` datetime DEFAULT NULL,
+  `max` int(6) DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `responses` (
+  `registrationid` int(9) NOT NULL,
+  `userid` int(9) NOT NULL,
+  `appointmentid` int(9) NOT NULL,
+  `response` int(5) NOT NULL,
+  `responsed` datetime NOT NULL,
+  `changed` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `userid` int(9) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `lang` varchar(3) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appointmentid`);
+
+ALTER TABLE `responses`
+  ADD PRIMARY KEY (`registrationid`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
---
+++ demodata.sql
@@ -0,0 +1,23 @@
+
+INSERT INTO `appointment` (`appointmentid`, `userid`, `title`, `address`, `apptDate`, `created`, `updated`, `response`, `max`, `enabled`) VALUES
+(1, 1, 'Gormet', 'Restaurant Zirbelstube', '2020-09-29 12:30:00', '2020-09-26 00:32:15', NULL, NULL, NULL, 0),
+(2, 1, 'Spargelbesen', 'Besen Fellbach', '2020-09-23 12:30:00', '2020-09-22 00:32:15', NULL, NULL, NULL, 1),
+(3, 1, 'Spargelbesen', 'Besen Fellbach', '2020-10-22 12:30:00', '2020-09-22 00:32:15', NULL, NULL, 5, 1),
+(4, 1, 'Schweinshaxe', 'Brauereigasthof Karlsbrauerei', '2020-10-23 12:30:00', '2020-09-22 00:32:15', NULL, '2020-09-30 03:36:59', NULL, 1),
+(5, 1, 'Sushi', 'Sushi Le', '2020-10-06 12:30:00', '2020-09-22 00:32:15', NULL, '2020-09-26 00:37:28', NULL, 1);
+
+INSERT INTO `responses` (`registrationid`, `userid`, `appointmentid`, `response`, `responsed`, `changed`) VALUES
+(1, 1, 4, -1, '2020-09-26 02:43:42', NULL),
+(2, 1, 5, 1, '2020-09-26 02:43:42', NULL);
+
+INSERT INTO `users` (`userid`, `firstname`, `lastname`, `email`, `password`, `lang`, `active`, `admin`) VALUES
+(1, 'Demo', 'User', 'demo@user.com', 'fe01ce2a7fbac8fafaed7c982a04e229', 'de', 1, 1);
+
+ALTER TABLE `appointment`
+  MODIFY `appointmentid` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `responses`
+  MODIFY `registrationid` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `users`
+  MODIFY `userid` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
